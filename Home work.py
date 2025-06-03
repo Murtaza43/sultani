3a4c1a574be230cc51e4b50c8776cee1e9d3fa5a
Murtaza43 <m_sultani@kbtu.kz>
2025-06-03 11:36:36 -0700

    Add files via upload

---
+++ Home work.py
@@ -0,0 +1,604 @@
+import random
+import sys
+print | ('Hello, World!')
+
+
+print(sys.verstion)
+
+if 5 > 2:
+    print(" Five is greater than two!")
+
+if 5 > 2:
+print("Five is greater than two!")
+
+if 5 > 2:
+    print("Five is greater than two!")
+    if 5 > 2:
+        print("Five is greater than two!")
+
+x = 5
+y = "Hello, World!"
+
+print(x)
+print(y)
+
+# This is a comment.
+print("Hello, World!")
+
+print("Hello, World!")  # This is a comment.
+
+# print("Hello, World!")
+print("Cheers, Mate!")
+
+# This is a comment
+# written in
+# more than just one line
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4
+x = "Sally"
+print(x)
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "John"
+
+print(type(x))
+print(type(y))
+
+x = "John"
+print(x)
+# double quotes are the same as singe quotes
+x = 'John'
+print(x)
+
+a = 4
+A = "Sally"
+
+print(a)
+print(A)
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+2myvar = "John"
+my-var = "John"
+my var = "John"
+
+# This example will produce an error in the result
+
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+
+print(x)
+print(y)
+print(z)
+
+x = "Python is awesome"
+print(x)
+
+x = "Python"
+y = "is"
+z = "awesome"
+
+print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "John"
+print(x + y)  # The output will be an error
+
+x = 5
+y = "John"
+print(x, y)
+
+x = "awesome"
+
+
+def myfunc():
+    print("Python is " + x)
+
+
+myfunc()
+
+x = "awesome"
+
+
+def myfunc():
+    x = "fantastic"
+    print("Python is " + x)
+
+
+myfunc()
+
+print("Python is " + x)
+
+x = "awesome"
+
+
+def myfunc():
+    global x
+    x = "fantastic"
+
+
+myfunc()
+
+print("Python is " + x)
+
+x = 5
+print(type(x))
+
+x = 1j
+
+# display the value of x
+print(x)
+
+# display the data type of x
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = range(6)
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = {"name": "John", "age": 36}
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = {"apple", "banana", "cherry"}
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = frozenset({"apple", "banana", "cherry"})
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = True
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = b"Hello"
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = bytearray(5)
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = memoryview(bytes(5))
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = None
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = str("Hello World")
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = int(20)
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = float(20.5)
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = complex(1j)
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = list(("apple", "banana", "cherry"))
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = tuple(("apple", "banana", "cherry"))
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = range(6)
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = dict(name="John", age=36)
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = set(("apple", "banana", "cherry"))
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = frozenset(("apple", "banana", "cherry"))
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = bool(5)
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = bytes(5)
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = bytearray(5)
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = memoryview(bytes(5))
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+
+x = 1
+y = 2.8
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# convert from int to float:
+x = float(1)
+
+# convert from float to int:
+y = int(2.8)
+
+# convert from int to complex:
+z = complex(1)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+print(random.randrange(1, 10))
+
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
+
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+
+# we can use double or single quotes:
+
+print("Hello")
+print('Hello')
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = "Hello"
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+# The same as above
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+a = "Hello, World!"
+print(a[0])
+
+for x in "banana":
+    print(x)
+
+a = "Hello, World!"
+print(len(a))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+    print("Yes, 'free' is present.")
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+    print("No, 'expensive' is not present.")
+
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+b = "Hello, World!"
+print(b[2:])
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip())
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello, World!"
+print(a.split(","))
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+age = 36
+txt = "My name is John, I am " + age
+print(txt)
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+price = 37
+txt = f"The price is {price} dollars"
+print(txt)
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+txt = f"The price is {13 * 15} dollars"
+print(txt)
+
+txt = "We are the so-called "Vikings" from the north."
+
+
+# You will get an error if you use double quotes inside a string that are surrounded by double quotes:
+
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
+
+# Other escape characters used in Python
+txt = 'It\'s alright.'
+print(txt)
+
+txt = "This will insert one \\ (backslash)."
+print(txt)
+
+txt = "Hello\nWorld!"
+print(txt)
+
+txt = "Hello\rWorld!"
+print(txt)
+
+txt = "Hello\tWorld!"
+print(txt)
+
+# This example erases one character (backspace):
+txt = "Hello \bWorld!"
+print(txt)
+
+# A backslash followed by three integers will result in a octal value:
+txt = "\110\145\154\154\157"
+print(txt)
+
+# A backslash followed by an 'x' and a hex number represents a hex value:
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt)
